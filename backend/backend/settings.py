ec6cae404bb50f41ca3ba9ea248de035963cdfa7
Vladislav Panyshko <panyshko.v@yandex.ru>
2023-07-27 00:50:10 +0300

    settings.py поправил ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,11 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1',
+                 '62.84.121.60',
+                 'hostnameforlearning.hopto.org'
+                 ]
 
 
 # Application definition
